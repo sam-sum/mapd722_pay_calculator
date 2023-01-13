2c0af180b6087433ac6345b024b6d2517cf82a2b
sam-sum <113701007+sam-sum@users.noreply.github.com>
2023-01-13 18:10:34 -0500

    Fixed provider.of and consumer

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 class MainModel extends ChangeNotifier {
-  static final MainModel _mainModel = MainModel._internal();
-  factory MainModel() {
-    return _mainModel;
-  }
-
-  MainModel._internal();
-
-  static double _hours = 0;
-  static double _rates = 0;
+  double _hours = 0;
+  double _rates = 0;
 
   double get hours => _hours;
   double get rates => _rates;
@@ -46,17 +39,20 @@ class MyApp extends StatelessWidget {
       onTap: () {
         FocusManager.instance.primaryFocus?.unfocus();
       },
-      child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text("MAPD722 Pay Calculator"),
-          ),
-          body: Column(
-            children: const [
-              FormPanel(),
-              OutputPanel(),
-              AboutPanel(),
-            ],
+      child: ChangeNotifierProvider<MainModel>(
+        create: (BuildContext context) => MainModel(),
+        child: MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(
+              title: const Text("MAPD722 Pay Calculator"),
+            ),
+            body: Column(
+              children: const [
+                FormPanel(),
+                OutputPanel(),
+                AboutPanel(),
+              ],
+            ),
           ),
         ),
       ),
@@ -78,95 +74,92 @@ class _FormPanelState extends State<FormPanel> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => MainModel(),
-      child: Consumer<MainModel>(builder: (context, model, child) {
-        return Container(
-          margin: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
-          padding: const EdgeInsets.fromLTRB(50.0, 20.0, 50.0, 20.0),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              border: Border.all(
-                color: Colors.blueAccent,
-                width: 2,
-              )),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                TextFormField(
-                  controller: hoursController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8)),
-                    labelText: "Number of hours",
-                    labelStyle: const TextStyle(fontSize: 16.0),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter the hours worked';
-                    } else if (double.parse(value) < 0) {
-                      return 'Please enter a positive value';
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  controller: ratesController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8)),
-                    labelText: "Hourly rates",
-                    labelStyle: const TextStyle(fontSize: 16.0),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter the hourly rate';
-                    } else if (double.parse(value) < 0) {
-                      return 'Please enter a positive value';
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 40),
-                  height: 50.0,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: const StadiumBorder(),
-                    ),
-                    onPressed: () {
-                      FocusManager.instance.primaryFocus?.unfocus();
-                      if (_formKey.currentState?.validate() ?? false) {
-                        model.hours = double.parse(hoursController.text);
-                        model.rates = double.parse(ratesController.text);
-                        print(
-                            'form is valid to submit ${model.hours}, ${model.rates}');
-                      }
-                    },
-                    child: const Text(
-                      'Calculate',
-                      style: TextStyle(fontSize: 18.0),
-                    ),
-                  ),
-                ),
-              ],
+    return Container(
+      margin: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
+      padding: const EdgeInsets.fromLTRB(50.0, 20.0, 50.0, 20.0),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            color: Colors.blueAccent,
+            width: 2,
+          )),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextFormField(
+              controller: hoursController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                labelText: "Number of hours",
+                labelStyle: const TextStyle(fontSize: 16.0),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the hours worked';
+                } else if (double.parse(value) < 0) {
+                  return 'Please enter a positive value';
+                } else {
+                  return null;
+                }
+              },
             ),
-          ),
-        );
-      }),
+            const SizedBox(
+              height: 20.0,
+            ),
+            TextFormField(
+              controller: ratesController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                labelText: "Hourly rates",
+                labelStyle: const TextStyle(fontSize: 16.0),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the hourly rate';
+                } else if (double.parse(value) < 0) {
+                  return 'Please enter a positive value';
+                } else {
+                  return null;
+                }
+              },
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              height: 50.0,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: const StadiumBorder(),
+                ),
+                onPressed: () {
+                  FocusManager.instance.primaryFocus?.unfocus();
+                  if (_formKey.currentState?.validate() ?? false) {
+                    Provider.of<MainModel>(context, listen: false).hours =
+                        double.parse(hoursController.text);
+                    Provider.of<MainModel>(context, listen: false).rates =
+                        double.parse(ratesController.text);
+                    print(
+                        'form is valid to submit ${hoursController.text}, ${ratesController.text}');
+                  }
+                },
+                child: const Text(
+                  'Calculate',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -184,46 +177,42 @@ class _OutputPanelState extends State<OutputPanel> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => MainModel(),
-      child: Consumer<MainModel>(builder: (context, model, child) {
-        return Expanded(
-          child: Container(
-              width: double.infinity,
-              margin: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
-              padding: const EdgeInsets.fromLTRB(20.0, 50.0, 20.0, 20.0),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  border: Border.all(
-                    color: Colors.blueAccent,
-                    width: 2,
-                  )),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Report',
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
+    return Expanded(
+      child: Container(
+          width: double.infinity,
+          margin: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+          padding: const EdgeInsets.fromLTRB(20.0, 50.0, 20.0, 20.0),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(
+                color: Colors.blueAccent,
+                width: 2,
+              )),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Text(
+                'Report',
+                style: TextStyle(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Consumer<MainModel>(builder: (context, model, child) {
+                return Text(
                     model.hours == 0 && model.rates == 0
                         ? reportEmpty
                         : getReportContent(model.hours, model.rates),
                     maxLines: 4,
-                    style: const TextStyle(fontSize: 20.0),
-                  ),
-                ],
-              )),
-        );
-      }),
+                    style: const TextStyle(fontSize: 20.0));
+              }),
+            ],
+          )),
     );
   }
 
